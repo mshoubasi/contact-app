2c78cd23d5bad008367785c0450c135013fe0508
mshoubasi <mhd.shoubasi@gmail.com>
2023-09-25 15:24:57 +0300

    added person routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PersonController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::controller(PersonController::class)->prefix('person')->name('person')->middleware('auth')->group(function(){
+    Route::get('/', 'index')->name('.index');
+    Route::get('/create', 'create')->name('.create');
+    Route::post('/store', 'store')->name('.store');
+    Route::get('/{person}/edit', 'edit')->name('.edit');
+    Route::put('/{person}/update', 'update')->name('.update');
+    Route::delete('/{person}/destroy', 'destroy')->name('.destroy');
 });
 
 require __DIR__.'/auth.php';
